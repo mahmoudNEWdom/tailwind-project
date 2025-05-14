0255418fa71b925ccb0baae089034fc7b368065b
mahmoudNEWdom <pprokiller2@gmail.com>
2025-05-14 18:47:01 +0300

    Add initial project setup with components, assets, and configuration files
    - Created Header and Footer components with basic structure and styling.
    - Added Home page component with a welcome message.
    - Included SVG and image assets for branding and UI elements.
    - Set up Tailwind CSS for styling and Vite for development environment.
    - Configured index.css for global styles and imported necessary dependencies in main.jsx.
    - Established tailwind.config.js for Tailwind CSS configuration.
    - Added favicon and various icons for UI.

---
+++ src/components/Header.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import logo from '../assets/logo.svg';
+import { useRef } from 'react';
+
+const Header = () => {
+
+    const headerRef = useRef()
+    const [Links,setLinks] = useState(["Features" ,"Team" ,"Signin"])
+    useEffect(() => {
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 100) {
+                headerRef.current.style.background = "#0c1524";
+                headerRef.current.style.padding = "20px 0";
+            } else {
+                headerRef.current.style.background = "transparent";
+                headerRef.current.style.padding = "60px 0";
+            }
+        });
+
+    }, [])
+
+    return (
+    <header className='pt-[60px] fixed top-0 left-0 w-full z-50 transition-all duration-200' ref={headerRef}>
+        <div className='container mx-auto flex items-center justify-between flex-col gap-4 sm:flex-col md:flex-row'>
+        <img src={logo} alt="Logo" />
+        <nav>
+            <ul className='flex items-center justify-center gap-9 '>
+            {Links.map((l) => {
+                return (
+                    <li key={l} >
+                        <a href={`/${l.toLowerCase()}`} className='text-white opacity-90 hover:opacity-100 hover:underline transition-opacity duration-500'>{l}</a>
+                    </li>
+                );
+            })}
+            </ul>
+        </nav>
+        </div>
+    </header>
+    );
+}
+
+export default Header;
